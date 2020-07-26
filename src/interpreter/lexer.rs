7a6e0c030cbc7a4159ef31747bae99ca0f70935f
Mithun <mithunrathinasamy@live.com>
2020-07-26 22:27:24 +1000

    feat(lexer): add lexer

--- src/interpreter/lexer.rs
+++ src/interpreter/lexer.rs
@@ -1,11 +1,11 @@
 use std::slice::Iter;
 use std::iter::Peekable;
 
-#[derive(Debug, Copy, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Token {
     Number(f64),
     Parenthesis(char),
-    Symbol(&'static str),
+    Symbol(String),
 }
 
 #[allow(unused)]
@@ -23,6 +23,51 @@ impl<'a> Iterator for Lexer<'a> {
     type Item = Token;
 
     fn next(&mut self) -> Option<Self::Item> {
+        
+        while let Some(&&ch) = self.code.peek() {
+            match ch {
+                ch if ch.is_ascii_digit() => {
+                    let mut number = String::new();
+
+                    while let Some(&&ch) = self.code.peek() {
+                        if ch.is_ascii_digit() || ch == '.' {
+                            self.code.next();
+                            number.push(ch);
+                        } else {
+                            break;
+                        }
+                    }
+
+                    if let Ok(number) = number.parse::<f64>() {
+                        return Some(Token::Number(number));
+                    } else {
+                        panic!(format!("invalid number: {}", number));
+                    }
+                },
+                '(' | ')' => {
+                    self.code.next();
+                    return Some(Token::Parenthesis(ch));
+                },
+                _ => {
+                    let mut symbol = String::new();
+
+                    while let Some(&ch) = self.code.next() {
+                        if ch.is_ascii_whitespace() {
+                            break;
+                        } else {
+                            symbol.push(ch);
+                        }
+                    }
+
+                    if symbol.len() == 0 {
+                        continue;
+                    }
+
+                    return Some(Token::Symbol(symbol));
+                },
+            }
+        }
+
         None
     }
 }
@@ -49,7 +94,7 @@ mod tests {
     fn test_basic_expr() {
         lex_and_assert_eq!("(+ 2 74.95)", vec![
             Token::Parenthesis('('),
-            Token::Symbol("+"),
+            Token::Symbol("+".to_string()),
             Token::Number(2.0),
             Token::Number(74.95),
             Token::Parenthesis(')'),
@@ -57,24 +102,28 @@ mod tests {
     }
 
     #[test]
-    fn test_all_valid_symbols() {
-        lex_and_assert_eq!("+ - * / %", vec![
-            Token::Symbol("+"),
-            Token::Symbol("-"),
-            Token::Symbol("*"),
-            Token::Symbol("/"),
-            Token::Symbol("%"),
+    fn test_missing_space_merges_number_to_symbol() {
+        lex_and_assert_eq!("(+2 4)", vec![
+            Token::Parenthesis('('),
+            Token::Symbol("+2".to_string()),
+            Token::Number(4.0),
+            Token::Parenthesis(')'),
         ]);
     }
 
     #[test]
-    #[should_panic(expected = "unrecognized symbol")]
-    fn test_invalid_symbol() {
-        lex_and_assert_eq!("(+2 3)", Vec::<_>::new());
+    fn test_all_valid_symbols() {
+        lex_and_assert_eq!("+ - * / %", vec![
+            Token::Symbol("+".to_string()),
+            Token::Symbol("-".to_string()),
+            Token::Symbol("*".to_string()),
+            Token::Symbol("/".to_string()),
+            Token::Symbol("%".to_string()),
+        ]);
     }
 
     #[test]
-    #[should_panic(expected = "invalid number")]
+    #[should_panic(expected = "invalid number: 98.23.35")]
     fn test_invalid_number() {
         lex_and_assert_eq!("98.23.35", Vec::<_>::new());
     }
--- src/repl/mod.rs
+++ src/repl/mod.rs
@@ -1,4 +1,5 @@
 use rustyline::{Editor, error::ReadlineError};
+use crate::interpreter::lexer::{Lexer, Token};
 
 pub fn run() {
     println!("Welcome to LISP Calc v0.1\n");
@@ -10,10 +11,16 @@ pub fn run() {
             Ok(line) => {
                 let line = line.trim();
                 rl.add_history_entry(line);
-                println!("{}", line);
+                print_output(line);
             },
             Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => break,
             Err(err) => eprintln!("{:?}", err),
         }
     }
+}
+
+fn print_output(input: &str) {
+    let input = input.chars().collect::<Vec<char>>();
+    let lexer = Lexer::new(input.iter());
+    println!("{:?}", lexer.collect::<Vec<Token>>());
 }
